6f3e9e7db88797cf9a2f32d11fe90957eb898078
Hemanth Kumar Giduturi <134470930+HeMaNtH624@users.noreply.github.com>
2025-12-24 14:17:10 +0530

    Add files via upload

---
+++ DSA/linked_list/single_list/reverse_list.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+struct st
+{
+	int roll;
+	char name[20];
+	float marks;
+	struct st *next;
+};
+
+void add_begin(struct st**);
+void print(struct st*);
+void save(struct st*);
+
+int main()
+{
+	char op;
+	struct st* hptr=0;
+	do
+	{
+
+	}
+}
