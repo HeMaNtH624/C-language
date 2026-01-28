dff87298af410824498cd0b0a289263352020655
Hemanth Kumar Giduturi <134470930+HeMaNtH624@users.noreply.github.com>
2026-01-28 13:55:19 +0530

    Add files via upload

---
+++ recursion_functions/prime.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+void prime(int);
+int main()
+{
+	int n;
+	int j;
+	for(j=2; j<1000; j++)
+	prime(j);
+	printf("\n");
+}
+
+void prime(int n)
+{
+	
+	int i = 2; count = 0;
+	while(i<=n/2)
+	{
+		if(n%i==0)
+			count++;
+		i++;
+		//prime(n, i, count);
+	}
+	//else
+	//{
+		if(count==0) 
+			printf("%d ",n);
+	//}
+	
+
+}
