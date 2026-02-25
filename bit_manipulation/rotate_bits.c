cd86db555ce4bb4f72196c359a2aab653f4045c3
Hemanth Kumar Giduturi <134470930+HeMaNtH624@users.noreply.github.com>
2026-02-25 18:16:28 +0530

    Add files via upload

--- bit_manipulation/rotate_bits.c
+++ bit_manipulation/rotate_bits.c
@@ -2,7 +2,7 @@
 int main()
 {
 	int num,res, lr, n;
- 	printf("Enter the number: ");
+	printf("Enter the number: ");
 	scanf("%d",&num);
 
 	for(int i=31; i>=0; i--)
@@ -10,7 +10,7 @@ int main()
 		printf("%d",(num>>i)&1);
 	}
 	
- 	printf("\n");
+	printf("\n");
 	printf("Enter number of bits to rotate: ");
 	scanf("%d",&n);
 
@@ -21,7 +21,7 @@ int main()
 	{
 		res = (num<<n) | (num>>(32-n));
 	}
- 
+
 	else if(lr==2)
 	{
 		res = (num>>n) | (num<<(32-n));
